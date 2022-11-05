657454edb7cea026d847aa3a91755474ab121d3e
Eliel Souza <eliellsouzza@gmail.com>
2022-11-05 18:13:15 -0300

    exercícios Labenu

---
+++ variaveis/script.js
@@ -0,0 +1,30 @@
+const nomeObjeto ="AppMusica"
+let qtdplaylist = 5
+let playlistAutoral = false
+
+
+console.log (typeof nomeObjeto)
+console.log (typeof qtdplaylist)
+console.log (typeof playlistAutoral)
+
+console.log (nomeObjeto)
+console.log (qtdplaylist)
+console.log (playlistAutoral)
+
+
+//exercicio 2
+
+
+let a = 30
+
+let b = 20 
+
+let c = null 
+
+
+c = a 
+a = b 
+b = c 
+
+console.log (a)
+console.log (b)
